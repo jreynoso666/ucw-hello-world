3c8dcffe45066296b759e1abba97202738b38fb7
Juan Reynoso Elias <juanreynoso@ikkiware.com>
2017-07-14 12:18:48 -0500

    Add new files.

---
+++ wwwroot/js/site.js
@@ -0,0 +1,14 @@
+function displayImage() {
+    var content = $("#content-img");
+    $.ajax({url: "get-image",
+	    data: {"type": this.statusgoal},
+	    cache: false,
+	    type: "post",
+	    success: function(result){
+		content.html(result);
+	    },
+	    error: function(xhrm, status){
+
+	    }
+	   });
+}
