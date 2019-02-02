afe392c81e452549bb79578a55c6387b35b06108
Juan Reynoso Elias <juanreynoso@ikkiware.com>
2019-02-02 09:21:38 -0600

    Added Bootstrap toast example.

--- examples-bootstrap-4.0.0.lisp
+++ examples-bootstrap-4.0.0.lisp
@@ -1,8 +1,7 @@
 (in-package #:ucw-hello-world)
 
-
 ;; define the entry point
-(defentry-point "bootstrap/examples/pricing" (:application *ucw-hello-world-app*)
+(defentry-point "examples/pricing" (:application *ucw-hello-world-app*)
     ()
   ;; display the window component
   (call 'bootstrap-4))
@@ -14,21 +13,20 @@
       :title "UCW hello world"
     :meta '((:name "viewport" :content "width=device-width, initial-scale=1, shrink-to-fit=no"))
     :javascript '((:src  "https://code.jquery.com/jquery-3.3.1.min.js")
-		  (:src  "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js")
+		  (:src  "https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js")
 		  (:src "wwwroot/js/site.js"))
-    :stylesheet '("https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css")
+    :stylesheet '("https://use.fontawesome.com/releases/v5.0.12/css/all.css"
+		  "https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css")
     :body (make-instance 'pricing)))
 
 (defcomponent pricing ()
   ())
 
 (defmethod render ((component pricing))
-  
   (<:div :class "container"
-	 
 	 (<:h4 :class "text-white text-center mb-2 py-2"
-		      :style "background-color:#2595ff; height:50px;"
-		      "Bootstrap 4 Examples")
+	       :style "background-color:#2595ff; height:50px;"
+	       "Bootstrap 4 Examples")
 	 (<:div :class "row"
 		(<:div :class "col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4"
 		       (<:div :class "card text-center mb-4"
@@ -46,10 +44,8 @@
 					   (<:li "Email support")
 					   (<:li "Help center access"))
 				     (<:button :type "button"
-					       :class "btn btn-lg btn-block btn-outline-primary"
-					       "Sign up for free"
-					       ))))
-
+					       :class "btn btn-lg btn-block btn-outline-secondary"
+					       "Sign up for free"))))
 		(<:div :class "col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4"
 		       (<:div :class "card text-center mb-4"
 			      (<:div :class "card-header"
@@ -67,8 +63,7 @@
 					   (<:li "Help center access"))
 				     (<:button :type "button"
 					       :class "btn btn-lg btn-block btn-primary"
-					       "Get started"
-					       ))))
+					       "Get started"))))
 		(<:div :class "col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4"
 		       (<:div :class "card text-center mb-4"
 			      (<:div :class "card-header"
@@ -86,5 +81,53 @@
 					   (<:li "Help center access"))
 				     (<:button :type "button"
 					       :class "btn btn-lg btn-block btn-success"
-					       "Get started"
-					       )))))))
+					       "Get started")))))))
+
+(defentry-point "components/toasts" (:application *ucw-hello-world-app*)
+    ()
+  ;; display the window component
+  (call 'bootstrap-toast))
+
+;; define the window component
+(defcomponent bootstrap-toast (standard-window-component)
+  ()
+  (:default-initargs
+      :title "Bootstrap toast"
+    :meta '((:name "viewport" :content "width=device-width, initial-scale=1, shrink-to-fit=no"))
+    :javascript '((:src  "https://code.jquery.com/jquery-3.3.1.min.js")
+		  (:src  "https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"))
+    :stylesheet '("https://use.fontawesome.com/releases/v5.0.12/css/all.css"
+		  "https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css")
+    :body (make-instance 'toast)))
+
+(defcomponent toast ()
+  ())
+
+(defmethod render ((component toast))
+  (<:div :class "container"
+	 (<:h4 :class "text-white text-center mb-2 py-2"
+	       :style "background-color:#2595ff; height:50px;"
+	       "Bootstrap 4.2 Components toast")
+	 (<:h4 :class "py-3"
+	  "Push notifications to your visitors with a toast, a lightweight and easily customizable alert message.")
+   (<:div :class "toast"
+	  :role "alert"
+	  :aria-alive "assertive"
+	  :aria-atomic "true"
+	  :data-autohide "false"
+	  (<:div :class "toast-header"
+		 (<:strong :class "mr-auto"
+			   "Bootstrap ")
+		 (<:small "11 mins ago")
+		 (<:button :class "ml-2 mb-1 close"
+			   :data-dismiss "toast"
+			   :aria-label "Close"
+			   (<:span  :arial-hidden "true"
+				    (<:i :class "fa fa-times"))))
+	  (<:div :class "toast-body"
+		 "Hello, world! This is a toast message."))
+   (<:a :href "https://getbootstrap.com/docs/4.2/components/toasts/"
+       "Bootstrap v4.2 toast"))
+  ;; Methods of the toast
+  (<:script :type "text/javascript"
+	    "$('.toast').toast('show')"))
